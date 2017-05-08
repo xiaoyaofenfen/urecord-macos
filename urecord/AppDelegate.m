5a09d5ff5a9bfc03a76f6575979be3ed39c0c9fa
liangfen@iMac <liangfen1206@gmail.com>
2017-05-08 23:33:15 +0800

    Title :complete main feature
    Description :

--- urecord/AppDelegate.m
+++ urecord/AppDelegate.m
@@ -9,7 +9,7 @@
 #import "AppDelegate.h"
 #import <AVFoundation/AVFoundation.h>
 
-@interface AppDelegate ()<AVCaptureFileOutputDelegate, AVCaptureFileOutputRecordingDelegate>
+@interface AppDelegate ()<AVCaptureFileOutputDelegate, AVCaptureFileOutputRecordingDelegate, NSWindowDelegate>
 
 
 @property (weak) IBOutlet NSWindow *window;
@@ -21,6 +21,8 @@
 @property (retain) AVCaptureAudioFileOutput *audioFileOutput;
 @property (retain) AVCaptureSession *session;
 @property (retain) AVAudioPlayer *player;
+@property BOOL needToPlayAfterStopRecord, needToRecordAfterStopRecord, needToRemoveRecordAfterStopRecord;
+@property (retain) NSString *recordTmpFilePath, *recordTmpFolderPath;
 
 - (IBAction)beginToRecord:(id)sender;
 - (IBAction)beginToReplay:(id)sender;
@@ -32,6 +34,8 @@
 
 @synthesize recordButton, replayButton, timeLabel;
 @synthesize audioDeviceInput, audioFileOutput, session, player;
+@synthesize needToPlayAfterStopRecord, needToRecordAfterStopRecord, needToRemoveRecordAfterStopRecord;
+@synthesize recordTmpFilePath, recordTmpFolderPath;
 
 -(instancetype) init
 {
@@ -40,6 +44,34 @@
     {
         NSLog(@"init the AppDelegate");
         [self initAudioRecord];
+        [self initAudioPlayer];
+        needToPlayAfterStopRecord = FALSE;
+        needToRecordAfterStopRecord = FALSE;
+        needToRemoveRecordAfterStopRecord = FALSE;
+
+        NSFileManager *fm = [NSFileManager defaultManager];
+        NSURL *desktopURL = [NSURL fileURLWithPath:@"/"
+                                       isDirectory:YES];
+        NSError *error = nil;
+        NSURL *temporaryDirectoryURL = [[NSFileManager defaultManager] URLForDirectory:NSItemReplacementDirectory
+                                                                              inDomain:NSUserDomainMask
+                                                                     appropriateForURL:desktopURL
+                                                                                create:NO
+                                                                                 error:&error];
+        recordTmpFolderPath = [temporaryDirectoryURL path];
+        if ([fm fileExistsAtPath:recordTmpFolderPath]) {
+            [fm removeItemAtPath:recordTmpFolderPath error:nil];
+        }
+        NSRange range = [recordTmpFolderPath rangeOfString:@"/" options:NSBackwardsSearch];
+        NSRange newStringRange;
+        newStringRange.location = 0;
+        newStringRange.length = range.location;
+        recordTmpFolderPath = [recordTmpFolderPath substringWithRange:newStringRange];
+
+        if (![fm fileExistsAtPath:recordTmpFolderPath]) {
+            [fm createDirectoryAtPath:recordTmpFolderPath withIntermediateDirectories:YES attributes:nil error:nil];
+        }
+        recordTmpFilePath = [NSString stringWithFormat:@"%@/%@", recordTmpFolderPath, @"test.m4a"] ;
     }
     
     return self;
@@ -67,6 +99,35 @@
     [[self session] commitConfiguration];
 }
 
+- (void)initAudioPlayer {
+//    self.player = [AVAudioPlayer alloc];
+}
+
+- (void) playTheRecordFile: (NSString *)path {
+    NSFileManager *fm = [NSFileManager defaultManager];
+    if ( ![fm fileExistsAtPath:path] ) {
+        return;
+    }
+
+    NSLog(@"play the record file: %@", path);
+    player = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL URLWithString:path] fileTypeHint:AVFileTypeAppleM4A error:nil];
+    player.volume = 1.0f;
+    [player play];
+}
+
+- (void) recordTheFile: (NSString *)path {
+    NSLog(@"begin to record sound...");
+
+    NSFileManager *fm = [NSFileManager defaultManager];
+    if ([fm fileExistsAtPath:path]) {
+        [fm removeItemAtPath:path error:nil];
+    }
+    [session startRunning];
+    [[self audioFileOutput] startRecordingToOutputFileURL:[NSURL fileURLWithPath:path]
+                                           outputFileType:AVFileTypeAppleM4A
+                                        recordingDelegate:self];
+}
+
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
     // Insert code here to initialize your application
 }
@@ -76,22 +137,38 @@
 }
 
 - (IBAction)beginToRecord:(id)sender {
-    NSLog(@"begin to record sound...");
-
-    NSString *tempName = @"/Volumes/DataDisk/Music/test.m4a";
-    NSLog(@"%@",[AVCaptureAudioFileOutput availableOutputFileTypes]);
-
-    [session startRunning];
-    [[self audioFileOutput] startRecordingToOutputFileURL:[NSURL fileURLWithPath:tempName]
-                                           outputFileType:AVFileTypeAppleM4A
-                                        recordingDelegate:self];
+    if (player) {
+        NSLog(@"stop player");
+        if ([player isPlaying]) {
+            [player stop];
+        }
+    }
+    if ([session isRunning]) {
+        NSLog(@"stop preview record process...");
+        needToPlayAfterStopRecord = FALSE;
+        needToRecordAfterStopRecord = TRUE;
+        [audioFileOutput stopRecording];
+        [session stopRunning];
+    } else {
+        [self recordTheFile:recordTmpFilePath];
+    }
 }
 
 - (IBAction)beginToReplay:(id)sender {
     NSLog(@"beign to replay the record sound...");
-    [audioFileOutput stopRecording];
-    [session stopRunning];
-
+    if ([session isRunning]) {
+        needToPlayAfterStopRecord = TRUE;
+        needToRecordAfterStopRecord = FALSE;
+        [audioFileOutput stopRecording];
+        [session stopRunning];
+    } else {
+        if (player) {
+            if([player isPlaying]) {
+                [player stop];
+            }
+        }
+        [self playTheRecordFile:recordTmpFilePath];
+    }
 }
 
 - (void)captureOutput:(AVCaptureFileOutput *)captureOutput didStartRecordingToOutputFileAtURL:(NSURL *)fileURL fromConnections:(NSArray *)connections
@@ -109,11 +186,22 @@
 
         });
     } else {
+        NSLog(@"stop record and save the recording file");
 
-        // Move the recorded temporary file to a user-specified location
-        player = [[AVAudioPlayer alloc] initWithContentsOfURL:outputFileURL fileTypeHint:AVFileTypeAppleM4A error:nil];
-        player.volume = 1.0f;
-        [player play];
+        if (needToPlayAfterStopRecord) {
+            [self playTheRecordFile:[outputFileURL path]];
+
+        } else if (needToRecordAfterStopRecord) {
+            [self recordTheFile:[outputFileURL path]];
+
+        } else if (needToRemoveRecordAfterStopRecord) {
+            NSFileManager *fm = [NSFileManager defaultManager];
+            if ([fm fileExistsAtPath:recordTmpFolderPath]) {
+                NSLog(@"remove temp folder");
+                [fm removeItemAtPath:recordTmpFolderPath error:nil];
+            }
+        }
+
     }
 }
 
@@ -121,6 +209,33 @@
 - (void)awakeFromNib {
     NSLog(@"awakeFromNib, init components");
 
+}
+
+- (void)windowWillClose:(NSNotification *)notification {
+    NSLog(@"window will close, release the resource...");
+
+    if (player) {
+        NSLog(@"stop player");
+        if ([player isPlaying]) {
+            [player stop];
+        }
+    }
+
+    if ([session isRunning]) {
+        NSLog(@"stop record");
+        needToPlayAfterStopRecord = FALSE;
+        needToRecordAfterStopRecord = FALSE;
+        needToRemoveRecordAfterStopRecord = TRUE;
+        [audioFileOutput stopRecording];
+        [session stopRunning];
+
+    } else {
+        NSFileManager *fm = [NSFileManager defaultManager];
+        if ([fm fileExistsAtPath:recordTmpFolderPath]) {
+            NSLog(@"remove temp folder");
+            //[fm removeItemAtPath:recordTmpFolderPath error:nil];
+        }
+    }
 }
 
 @end
